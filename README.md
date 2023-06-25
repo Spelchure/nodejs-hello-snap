73eb3460db5879df8602613be30707c91cb79b6a
Spelchure <alperenbayramoglu2@gmail.com>
2023-06-25 10:46:45 +0300

    initial: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple NodeJS Snap Package Example
+
+See: [Quick example creating snap for nodejs]()
---
+++ main.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+function helloSnap() {
+  console.log("This is my first NodeJS snap application.");
+}
+
+helloSnap();
